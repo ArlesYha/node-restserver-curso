0e369dd4a846b0f9610b02e8a0d82278af0f3638
ArlesYha <arlesyhazelada98@gmail.com>
2020-11-02 23:43:08 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,19 @@
 //Puerto
 
 process.env.PORT = process.env.PORT || 3000
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || "dev"
+
+let urlDB;
+
+if(process.env.NODE_ENV === "dev") {
+    urlDB = "mongodb://localhost:27017/cafe"
+}else {
+    urlDB = "mongodb+srv://ArlesYha:AFSxccI8nmemLgEj@cluster0.jarop.mongodb.net/cafe?retryWrites=true&w=majority"
+}
+
+process.env.URLDB = urlDB
+
+// mongodb: //localhost:27017/cafe
+// mongodb: "mongodb+srv://ArlesYha:AFSxccI8nmemLgEj@cluster0.jarop.mongodb.net/<dbname>?retryWrites=true&w=majority"
